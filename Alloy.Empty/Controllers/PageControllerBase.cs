7cd51ad9059523316a95528a665dc548c1e6b517
pawsingh <pawan.singh@digitas.com>
2018-08-03 20:16:36 +0530

    init commit

---
+++ Alloy.Empty/Controllers/PageControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alloy.Empty.Models.Pages;
+using EPiServer.Core;
+using EPiServer.Shell.Security;
+using EPiServer.Web.Mvc;
+
+namespace Alloy.Empty.Controllers
+{
+    public abstract class PageControllerBase<T> : PageController<T>
+        where T : SitePageData
+    {
+
+        protected EPiServer.ServiceLocation.Injected<UISignInManager> UISignInManager;
+
+        //public ActionResult Logout()
+        //{
+        //    UISignInManager.Service.SignOut();
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
---
+++ Alloy.Empty/Controllers/StartPageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using Alloy.Empty.Models.Pages;
+
+namespace Alloy.Empty.Controllers
+{
+    public class StartPageController : PageControllerBase<Models.Pages.StartPage>
+    {
+        // GET: StartPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
